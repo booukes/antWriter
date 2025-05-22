8619d34759128625817875e6cfb40113480cf78a
booukes <lukaszandrzejpilat@gmail.com>
2025-05-22 02:28:13 +0200

    0.1.0-pre-alpha (see changelog)

---
+++ writingTools/RespButton.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace antWriter
+{
+    public partial class RespButton : UserControl
+    {
+        public RespButton() => InitializeComponent();
+
+        public static readonly DependencyProperty BtnBgProperty =
+            DependencyProperty.Register(nameof(BtnBg), typeof(Brush), typeof(RespButton), new PropertyMetadata(Brushes.Aqua));
+        public Brush BtnBg
+        {
+            get => (Brush)GetValue(BtnBgProperty);
+            set => SetValue(BtnBgProperty, value);
+        }
+
+        public static readonly DependencyProperty BtnTextProperty =
+            DependencyProperty.Register(nameof(BtnText), typeof(string), typeof(RespButton), new PropertyMetadata("Placeholder"));
+        public string BtnText
+        {
+            get => (string)GetValue(BtnTextProperty);
+            set => SetValue(BtnTextProperty, value);
+        }
+
+    public static readonly DependencyProperty BtnRadProperty =
+            DependencyProperty.Register(nameof(BtnRad), typeof(CornerRadius), typeof(RespButton), new PropertyMetadata(new CornerRadius(10)));
+
+        public CornerRadius BtnRad
+        {
+            get => (CornerRadius)GetValue(BtnRadProperty);
+            set => SetValue(BtnRadProperty, value);
+        }
+    }
+}
---
+++ writingTools/RespText.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace antWriter
+{
+    public partial class RespText : UserControl
+    {
+        public RespText()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(TextProp), typeof(string), typeof(RespText), new PropertyMetadata("Placeholder"));
+        public string TextProp
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+    }
+}
